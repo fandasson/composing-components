ae9550d5daf69c58b25d600b32878ce7a5a775c0
František Pác <5476595+fandasson@users.noreply.github.com>
2024-01-18 15:01:16 +0100

    feat: add `Logo` component

---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,7 @@
+export const Logo = () => (
+    <img
+        className="mx-auto h-28 w-auto rounded-full"
+        src="https://reactgirls.com/images/logo-blue.svg"
+        alt="React Girls"
+    />
+);
